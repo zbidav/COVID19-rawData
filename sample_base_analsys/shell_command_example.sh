7463805cc6bbd292c21c217017c2fd4c1c296c38
David Gorelik <59455718+zbidav@users.noreply.github.com>
2023-01-09 12:33:13 +0200

    Update shell_command_example.sh

--- sample_base_analsys/shell_command_example.sh
+++ sample_base_analsys/shell_command_example.sh
@@ -43,25 +43,24 @@ lzma -7 -T 30 /SRR13250339/SRR13250339ReadsPerGene.out.tab
 find SRR13250339 -name '*Unmapped.out.mate?'|xargs -I magicstring bash -c 'lzma -7 -T 30 magicstring'
 rm -r SRR13250339/SRR13250339_STARtmp
 
-```
-python "STARstatsV2.py" -r $output_directory -o $summeryFolder --plot -po $summeryFolder --force >> $cmdPath
-```
+#General script to combine STAR output
+python STARstatsV2.py -r STAR_dir/ -o STAR_summery_dir/ --plot -po STAR_summery_dir/  --force
+
 
 #SALMON
 mkdir -p SALMON/ResultFiles
-bash /home/alu/fulther/PipelineConfigs/BashSupport/runSalmon_1.4.0.sh -1 SRR13250339_1.fastq -2 SRR13250339_2.fastq -o SALMON/SRR13250339 -uc cat -l A -i /SALMON/index/hg38 -t 8 -g /SALMON/index/hg38/gencode_v32.transcriptToGeneID.tab --extra ""
+salmon1.4.0/salmon-latest_linux_x86_64/bin/salmon quant -l A -1 <(cat SRR13250339_1.fastq) -2 <(cat SRR13250339_2.fastq) -o SALMON/SRR13250339 -i /SALMON/index/hg38 -g /SALMON/index/hg38/gencode_v32.transcriptToGeneID.tab -p 8 
 mv SALMON/SRR13250339/quant.sf SALMON/SRR13250339/../ResultFiles/SRR13250339.quant.sf
 mv SALMON/SRR13250339/quant.quant.genes.sf SALMON/SRR13250339/../ResultFiles/SRR13250339.quant.genes.sf
 rm -r /private3/COVID_dbgap_Nasal/raw/Group1/SALMON_1.4.0/SRR13250339
  
-```
-IN_DIR="SALMON/ResultFiles/";
-OUT_DIR="SALMON/";
-mkdir -p $OUT_DIR;
-Rscript "uniteSalmonFIles.R" -i $IN_DIR -o $OUT_DIR --wide_genes --wide_samples 
+## General script to combine SALMON 
+Rscript "uniteSalmonFIles.R" -i SALMON/ResultFiles/ -o SALMON/ --wide_genes --wide_samples
 
-Rscript ZMAD_ScoreCalc_V2.R -i "SALMON/SalmonTPM.wideSamples.csv" -o "SALMON/" -p ArticleCalcLog2 --log2 
-```
+#calculation of ISG score (please see refence)
+## 'Calculate the ISG core score (mean absolute deviation modified Z-score) as specified in article (using their code): Tumor-derived IFN triggers chronic pathway agonism and sensitivity to ADAR loss (https://doi.org/10.1038/s41591-018-0302-5)
+Rscript ZMAD_ScoreCalc_V2.R -i "SALMON/SalmonTPM.wideSamples.csv" -o "SALMON/" --log2 
+
   
 #RNAEditingIndex
 ## we applied the tool using basal human paramaeters on all Alu elements
@@ -71,6 +70,7 @@ mkdir -p $OUT_DIR
 nohup RNAEditingIndex1.1 -d $IN_DIR -o $OUT_DIR -l $OUT_DIR -os $OUT_DIR --genome hg38 --paired_end --keep_cmpileup > ${OUT_DIR}/runAluIndex_Log.txt &
 
 ## we applied the tool using basal human paramaeters on 3'UTR Alu elements
+# the regioun obtained based on Cohen-Fultheim,R analsis. We defined Alu elements that have an opposite orientation Alu aside, with minimal length of 200. 
 IN_DIR="STAR/"
 OUT_DIR="RNAEditingIndex/Alu3pUTR/"
 REGIOUNS="hg38.Alu3pUTR_minLen200_17022021.RepeatsInOppositeOrientationAtRegions.sorted.merged.bed";
@@ -78,9 +78,10 @@ mkdir -p $OUT_DIR
 RNAEditingIndex1.1 -d $IN_DIR -o $OUT_DIR -l $OUT_DIR -os $OUT_DIR -rb $REGIOUNS --genome hg38 --paired_end --per_region_output --per_sample_output > ${OUT_DIR}/runAluIndex_Log.txt
 
 #REDI-Known
+# the file Orshay_AG_list.bed obtained from: Gabay, O., Shoshan, Y., Kopel, E. et al. Landscape of adenosine-to-inosine RNA recoding across human tissues. Nat Commun 13, 1184 (2022). https://doi.org/10.1038/s41467-022-28841-4
 cp Orshay_AG_list.bed known/SRR13250339/SRR13250339/SRR13250339known_sites_bed.txt
 
-python REDITOOLS/REDItools-1.0.4/reditools/REDItoolKnownHillel.py -i STAR/SRR13250339/SRR13250339Aligned.sortedByCoord.out.bam -f hg38.fa -l known/SRR13250339/SRR13250339/SRR13250339_sites_bed.txt -v 1 -n 0.001 -r 1 -T5-5 -c 20 -Q 33 -o known/SRR13250339/SRR13250339/SRR13250339_Folder
+python REDITOOLS/REDItools-1.0.4/reditools/REDItool.py -i STAR/SRR13250339/SRR13250339Aligned.sortedByCoord.out.bam -f hg38.fa -l known/SRR13250339/SRR13250339/SRR13250339_sites_bed.txt -v 1 -n 0.001 -r 1 -T5-5 -c 20 -Q 33 -o known/SRR13250339/SRR13250339/SRR13250339_Folder
 
 mv known/SRR13250339/SRR13250339/SRR13250339_Folder/*/outTable_* known/SRR13250339/SRR13250339/SRR13250339.REDItoolKnown.out.tab
 rm /known/SRR13250339/SRR13250339/SRR13250339known_sites_bed.txt*
